1de863246a355dfe82b0b0b93674851722e6c608
Jeffrey Lanters <jeffreylanters@gmail.com>
2018-07-30 15:34:43 +0200

    Create ECS.cs

---
+++ ECS.cs
@@ -0,0 +1,128 @@
+using System.Collections.Generic;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace UnityPackages.EntityComponentSystem {
+
+	public static class ECS {
+
+		/// <summary>
+		/// Controller.
+		/// </summary>
+		public class Controller : UnityEngine.MonoBehaviour {
+
+			public virtual void OnInitialize () { }
+
+			public static Controller Instance;
+
+			private List<ISystem> systems;
+
+			private void Awake () {
+				Instance = this;
+				this.systems = new List<ISystem> ();
+				this.OnInitialize ();
+			}
+
+			private void Update () {
+				for (var _i = 0; _i < this.systems.Count; _i++)
+					this.systems[_i].OnUpdate ();
+			}
+
+#if UNITY_EDITOR
+			private void OnDrawGizmos () {
+				if (UnityEngine.Application.isPlaying == true)
+					for (var _i = 0; _i < this.systems.Count; _i++)
+						this.systems[_i].OnDrawGizmos ();
+			}
+#endif
+
+			public void RegisterSystems (params ISystem[] systems) {
+				for (var _i = 0; _i < systems.Length; _i++) {
+					systems[_i].OnInitialize ();
+					this.systems.Add (systems[_i]);
+				}
+			}
+
+			public S GetSystem<S> () where S : ISystem, new () {
+				for (var _i = 0; _i < this.systems.Count; _i++)
+					if (this.systems[_i].GetType () == typeof (S))
+						return (S) this.systems[_i];
+				return new S ();
+			}
+		}
+
+		/// <summary>
+		/// System.
+		/// </summary>
+		public interface ISystem {
+			void OnInitialize ();
+			void OnUpdate ();
+			void OnDrawGizmos ();
+		}
+
+		public class System<S, C> : ISystem where S : System<S, C>, new () where C : Component<C, S>, new () {
+
+			public virtual void OnInitialize () { }
+			public virtual void OnUpdate () { }
+			public virtual void OnDrawGizmos () { }
+			public virtual void OnEntityInitialize (C component) { }
+
+			/// The list of instanced entities of this system.
+			public List<C> entities;
+
+			/// Gets the first entity instances by this system.
+			public C firstEntity { get { return this.entities[0]; } }
+
+			/// Creates a new system.
+			public System () {
+				this.entities = new List<C> ();
+			}
+
+			/// Adds an entity component to the system.
+			public void AddEntity (C component) {
+				this.entities.Add (component);
+				this.OnEntityInitialize (component);
+			}
+
+			/// Removes an entity component from the system.
+			public void RemoveEntry (C component) {
+				this.entities.Remove (component);
+			}
+
+			/// Gets a component from an entity.
+			public void GetComponentOnEntity<GEC> (C entity, System.Action<GEC> action) {
+				var _entity = entity.GetComponent<GEC> ();
+				if (_entity != null)
+					action (_entity);
+			}
+		}
+
+		/// <summary>
+		/// Component.
+		/// </summary>
+		public class Component<C, S> : UnityEngine.MonoBehaviour where C : Component<C, S>, new () where S : System<S, C>, new () {
+
+			private void Start () {
+				Controller.Instance.GetSystem<S> ().AddEntity ((C) this);
+			}
+
+			private void OnDestroy () {
+				Controller.Instance.GetSystem<S> ().RemoveEntry ((C) this);
+			}
+		}
+
+		public class Protected : UnityEngine.PropertyAttribute { }
+#if UNITY_EDITOR
+		[CustomPropertyDrawer (typeof (Protected))]
+		public class ProtectedPropertyDrawer : PropertyDrawer {
+			public override void OnGUI (UnityEngine.Rect position, SerializedProperty serializedProperty, UnityEngine.GUIContent label) {
+				EditorGUI.BeginDisabledGroup (true);
+				EditorGUI.PropertyField (position, serializedProperty, label, true);
+				EditorGUI.EndDisabledGroup ();
+			}
+		}
+#endif
+	}
+}
