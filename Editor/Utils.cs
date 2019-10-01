46d59d089333d0a67b5cfc8d10535c30cdfd2b66
Jeffrey Lanters <jeffrey@hulan.nl>
2019-10-01 15:40:48 +0200

    Changes Create File Util

--- Editor/Utils.cs
+++ Editor/Utils.cs
@@ -9,12 +9,12 @@ namespace UnityPackages.EntityComponentSystem {
   public static class Utils {
 
     public static void CreateFile (string title, string fileName, string afterSaveSuffix, string type, params string[] data) {
-      var _path = EditorUtility.SaveFilePanel (title, "Assets/", fileName, type);
+      var _path = EditorUtility.SaveFilePanel (title, "Assets/", fileName, "");
       if (_path.Length == 0)
         return;
       var _name = _path.Split ('/').Last ().Split ('.').First ();
       using (System.IO.StreamWriter outfile =
-        new System.IO.StreamWriter (_path.Replace ("." + type, afterSaveSuffix + "." + type))) {
+        new System.IO.StreamWriter (_path + afterSaveSuffix + "." + type)) {
         var _data = string.Join ("\n", data).Replace ("{{name}}", _name);
         outfile.Write (_data);
       }
