4c4274f1339afe66de4892094c5cc2c4387453ae
Jeffrey Lanters <jeffrey@hulan.nl>
2019-12-02 15:33:17 +0100

    Update EntityComponent.cs

--- Runtime/EntityComponent.cs
+++ Runtime/EntityComponent.cs
@@ -1,10 +1,14 @@
 namespace UnityPackages.EntityComponentSystem {
+  
+  // An entity component.
   public abstract class EntityComponent<C, S> : UnityEngine.MonoBehaviour where C : EntityComponent<C, S>, new () where S : EntitySystem<S, C>, new () {
 
     private bool isEntityEnabled = false;
     private bool isEntityInitialized = false;
     private S system = null;
 
+    /// Gets the system matched with this entity's component. If it's not
+    /// defined, it will be fetched from the controller.
     private S GetSystem () {
       if (this.system == null)
         if (Controller.Instance.HasSystem<S> () == true)
@@ -14,11 +18,13 @@ namespace UnityPackages.EntityComponentSystem {
       return this.system;
     }
 
-    private void Start () {
+    /// During the 'Start' the entity component will be registered to the
+    /// matching system and invokes 'OnEntityStart' on it.
+    private void Start () =>
       this.GetSystem ().InternalAddEntity ((C) this);
-      this.GetSystem ().OnEntityStart ((C) this);
-    }
 
+    /// During the 'Update' the entity component will invoke its 
+    /// 'OnEntityEnabled' and 'OnEntityInitialized' if needed.
     private void Update () {
       if (this.isEntityEnabled == false) {
         this.isEntityEnabled = true;
@@ -30,11 +36,15 @@ namespace UnityPackages.EntityComponentSystem {
       }
     }
 
+    /// During the 'OnDisabled' the entity component will invoke its
+    /// 'OnEntityDisabled' on the system.
     private void OnDisable () {
       this.isEntityEnabled = false;
       this.GetSystem ().OnEntityDisabled ((C) this);
     }
 
+    /// During the 'OnDestroy' the entity component will unregister it self
+    /// to the matching system.
     private void OnDestroy () =>
       this.GetSystem ().InternalRemoveEntry ((C) this);
   }
