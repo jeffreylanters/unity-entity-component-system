2a9fafe39bfb692bf6f85ed256c344ebf8c2bfab
Jeffrey Lanters <jeffrey@hulan.nl>
2019-12-13 10:57:03 +0100

    Minor refactoring

--- Runtime/Attributes/InjectedSystem.cs
+++ Runtime/Attributes/InjectedSystem.cs
@@ -5,13 +5,13 @@ namespace UnityPackages.EntityComponentSystem {
 
     /// Sets the attributes values on a system.
     public static void SetAttributeValues (IEntitySystem system) {
-      var _systemFields = system.GetType ().GetFields (
+      var _fields = system.GetType ().GetFields (
         System.Reflection.BindingFlags.Instance |
         System.Reflection.BindingFlags.NonPublic |
         System.Reflection.BindingFlags.Public);
-      foreach (var _systemField in _systemFields)
-        if (System.Attribute.GetCustomAttribute (_systemField, typeof (InjectedSystem)) != null)
-          _systemField.SetValue (system, Controller.Instance.GetSystem (_systemField.FieldType));
+      foreach (var _field in _fields)
+        if (System.Attribute.GetCustomAttribute (_field, typeof (InjectedSystem)) != null)
+          _field.SetValue (system, Controller.Instance.GetSystem (_field.FieldType));
     }
   }
 }
