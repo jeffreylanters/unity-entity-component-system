bb1bc940e4d1be5f4f98f771b089fab6bd9d16ae
Jeffrey Lanters <jeffrey@hulan.nl>
2020-03-02 11:11:33 +0100

    Injected systems now allow types of Object

--- Runtime/Attributes/InjectedSystem.cs
+++ Runtime/Attributes/InjectedSystem.cs
@@ -3,15 +3,15 @@ namespace UnityPackages.EntityComponentSystem {
   /// Describes a injectedable property within a system.
   public class InjectedSystem : System.Attribute {
 
-    /// Sets the attributes values on a system.
-    public static void SetAttributeValues (IEntitySystem system) {
-      var _fields = system.GetType ().GetFields (
+    /// Sets the attributes values on an object.
+    public static void SetAttributeValues (System.Object target) {
+      var _fields = target.GetType ().GetFields (
         System.Reflection.BindingFlags.Instance |
         System.Reflection.BindingFlags.NonPublic |
         System.Reflection.BindingFlags.Public);
       foreach (var _field in _fields)
         if (System.Attribute.GetCustomAttribute (_field, typeof (InjectedSystem)) != null)
-          _field.SetValue (system, Controller.Instance.GetSystem (_field.FieldType));
+          _field.SetValue (target, Controller.Instance.GetSystem (_field.FieldType));
     }
   }
 }
