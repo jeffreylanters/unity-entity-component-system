d60756edc0360f69a0f13fc2df1316e7fc08be0f
Jeffrey Lanters <jeffreylanters@gmail.com>
2020-04-16 22:30:08 +0200

    Protected fields are now hidden

---
+++ Runtime/Attributes/Protected.cs
@@ -0,0 +1,17 @@
+namespace UnityPackages.EntityComponentSystem {
+
+  /// Describes a protected property within a component.
+  [System.AttributeUsage (System.AttributeTargets.Field)]
+  public class Protected : UnityEngine.PropertyAttribute { }
+
+#if UNITY_EDITOR
+  [UnityEditor.CustomPropertyDrawer (typeof (Protected))]
+  public class ProtectedDrawer : UnityEditor.PropertyDrawer {
+    public override void OnGUI (UnityEngine.Rect position, UnityEditor.SerializedProperty serializedProperty, UnityEngine.GUIContent label) { }
+
+    public override float GetPropertyHeight (UnityEditor.SerializedProperty property, UnityEngine.GUIContent label) {
+      return 0;
+    }
+  }
+#endif
+}
