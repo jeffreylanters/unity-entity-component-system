2878c5580545300c09251060d63eb270aba9a367
Jeffrey Lanters <jeffreylanters@me.com>
2021-01-14 22:05:18 +0100

    Update EntityComponent.cs

--- Runtime/EntityComponent.cs
+++ Runtime/EntityComponent.cs
@@ -50,7 +50,7 @@ namespace ElRaccoone.EntityComponentSystem {
       UnityEngine.Object.Destroy (this.gameObject);
      
     /// Gets a component on an enity and sets it's reference to a property.
-    public void GetComponentToProperty<UnityComponentType> (ref UnityComponentType entityProperty, bool includeChildren = false, bool includeInactive) =>
+    public void GetComponentToProperty<UnityComponentType> (ref UnityComponentType entityProperty, bool includeChildren = false, bool includeInactive = false) =>
       entityProperty = includeChildren == true
         ? this.GetComponentInChildren<UnityComponentType> (includeInactive)
         : this.GetComponent<UnityComponentType> ();
